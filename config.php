461a8c1b7b3f4443813017a7995dbcec01ce666b
Adam Cox <adam.cox9@gmail.com>
2016-04-08 07:45:47 +0000

    put updates in config_safe to config for git

--- config.php
+++ config.php
@@ -1,11 +1,17 @@
 <?PHP
 
-/*	if( ! isset( $_SERVER['REMOTE_ADDR'] ) || $_SERVER['REMOTE_ADDR'] != '76.24.176.23' ) {
+	/*if( ! isset( $_SERVER['REMOTE_ADDR'] ) || $_SERVER['REMOTE_ADDR'] != '76.24.176.23' ) {
 		if( ! isset( $_SERVER['USER'] ) || $_SERVER['USER'] !== "root" ) {
 			header("HTTP/1.0 404 Not Found");
 			exit;
 		}
 	}*/
+
+	/*****
+
+		TODO - something with these requires:
+
+	 *****/
 
 	//Crypto tools, etc...
 	require_once( "adapters/crypto_interface.php" );
@@ -20,8 +26,9 @@
 	require_once( "bots/make_extreme_orders.php" );
 	require_once( "bots/make_max_orders.php" );
 	require_once( "bots/make_min_orders.php" );
+	require_once( "bots/run_tests.php" );
 
-	//Wrapper libraries for native API's:
+	//Wrapper libraries for native REST API's:
 	require_once( "adapters/bitfinex/bitfinex_lib.php" );
 	require_once( "adapters/bitstamp/bitstamp_lib.php" );
 	require_once( "adapters/bittrex/bittrex_lib.php" );
@@ -40,33 +47,38 @@
 	require_once( "adapters/poloniex/poloniex_adapter.php" );
 
 	/*****
-		Globals are the best!
+		Globals are the best - need to do something to make this safer.
+		These can easily get leaked over the web with a print_r
 	 *****/
 
-	$bitfinex_api_key = "A4K0wCj6KLdChWkp2Xxd4xPLDWj9nYD7dCvdZ5Wj7jr";
-	$bitfinex_api_secret = "xYKT35o3rJloES7GWkO2lWKYH9c5kwxCVV0W3XqqaP7";
+	$bitfinex_api_key = "INSERT_API_KEY_HERE";
+	$bitfinex_api_secret = "INSERT_API_SECRET_HERE";
 
-	$bitstamp_api_key = "IFo7FPrSu97ufUbUS89CHz3uwqKyHFcX";
-	$bitstamp_api_secret = "CehjYgU0JArV9vtO7hj6p1WL5Q7rdVtP";
-	$bitstamp_api_number = "779882";
+	$bitstamp_api_key = "INSERT_API_KEY_HERE";
+	$bitstamp_api_secret = "INSERT_API_SECRET_HERE";
+	$bitstamp_api_number = "INSERT_API_NUMBER_HERE";
 
-	$bittrex_api_key = "5d46d0942fea4f059d95c3bce1377f57";
-	$bittrex_api_secret = "15aa417db72249d5831b402cab1aa289";
+	$bittrex_api_key = "INSERT_API_KEY_HERE";
+	$bittrex_api_secret = "INSERT_API_SECRET_HERE";
 
-	$btce_api_key = "0AKAY3GL-9ZPEYWH6-LYTBCROP-LGVA1GRY-80LTUVSI";
-	$btce_api_secret = "cdc6eef818df6c0edfc9647732cc68af96a25b9b554174b7c669a01e2607909a";
+	$btce_api_key = "INSERT_API_KEY_HERE";
+	$btce_api_secret = "INSERT_API_SECRET_HERE";
 
-	$bter_api_key = "EAD04357-77CE-4A75-9B7D-CFAD0B481D09";
-	$bter_api_secret = "1dd9b32a6a0a92ab70bc2944ad567db9b133593eee8c597d80e9d4392235f11b";
+	$bter_api_key = "INSERT_API_KEY_HERE";
+	$bter_api_secret = "INSERT_API_SECRET_HERE";
 
-	$coinbase_api_key = '8aa0beaec603603e90411f28f17b902f';
-	$coinbase_api_secret = '/2xL4nbN5jwjevO02WTyhRe3usqubtEJIlzL6+omWzls9cGHftdCtDfB7tQE1JwZkKGoaGtFrO6QhWxS47Kfgw==';
-	$coinbase_api_passphrase = '8f0ac5kewjtw3ik9';
+	$coinbase_api_key = "INSERT_API_KEY_HERE";
+	$coinbase_api_secret = "INSERT_API_SECRET_HERE";
+	$coinbase_api_passphrase = 'INSERT_API_PASSPHRASE_HERE';
 
-	$poloniex_api_key = "0LWESBZB-G42RZSC1-R1ODIXAS-D74LGT07";
-	$poloniex_api_secret = "876ef684bd13d3505d1c8c70179cc27b925e171bf6a4109f554f7472776d16a8331156ed9bc50acd977ef470120d36f9e742a4f7a0f57a415149d330aa7ab31d";
+	$poloniex_api_key = "INSERT_API_KEY_HERE";
+	$poloniex_api_secret = "INSERT_API_SECRET_HERE";
 
-	//_____IMPORTANT FOR YOU TO CHANGE!!! WE CAN'T GET DEPOSIT ADDRESS THROUGH API?
+	/*****
+		IMPORTANT FOR YOU TO CHANGE, NOT ME!!!
+		WE CAN'T GET DEPOSIT ADDRESS THROUGH API?
+	 *****/
+
 	$BTCE_BTC_DEPOSIT_ADDRESS = "1jPtEamiPHn2NaPXab29ruSAparsvrUre";
 	$BTCE_LTC_DEPOSIT_ADDRESS = "LZrNNQtK4yDzwEjj2VszEm529UaDDDsdPH";
 	$BTCE_NMC_DEPOSIT_ADDRESS = "NEtAMTUgqyD4w7DEA414PRSFjhoVJstP7W";
@@ -81,19 +93,6 @@
 	$Adapters['Bter'] = new BterAdapter( new Bter( $bter_api_key, $bter_api_secret ) );
 	$Adapters['Coinbase'] = new CoinbaseAdapter( new Coinbase( $coinbase_api_key, $coinbase_api_secret, $coinbase_api_passphrase ) );
 	$Adapters['Poloniex'] = new PoloniexAdapter( new Poloniex( $poloniex_api_key, $poloniex_api_secret ) );
-
-	$exchanges = [];
-	$currencies = [];
-	$markets = [];
-	$market_summaries = [];
-	$balances = [];
-	$open_orders = [];
-	$worths = [];
-	$completed_orders = [];
-	$deposit_addresses = [];
-	$trades = [];
-	$orderbooks = [];
-	$volumes = [];
 
 	$Tester = new Tester();
 
